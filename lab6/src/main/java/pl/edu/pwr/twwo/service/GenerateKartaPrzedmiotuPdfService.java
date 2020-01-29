cf69c5516703cb8d9fd082e79cb9bd4301cbda3d
Zofia Lakomiak <226190@student.pwr.edu.pl>
2020-01-29 23:11:01 +0100

    #1 GenerateKartaPrzedmiotuPdfService refactor pt1

--- lab6/src/main/java/pl/edu/pwr/twwo/service/GenerateKartaPrzedmiotuPdfService.java
+++ lab6/src/main/java/pl/edu/pwr/twwo/service/GenerateKartaPrzedmiotuPdfService.java
@@ -2,6 +2,8 @@ package pl.edu.pwr.twwo.service;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.edu.pwr.twwo.domain.*;
@@ -10,37 +12,24 @@ import pl.edu.pwr.twwo.repository.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InvalidObjectException;
 import java.util.Optional;
 import java.util.Set;
 
 @Service
 @Transactional
 public class GenerateKartaPrzedmiotuPdfService {
+    private final Logger log = LoggerFactory.getLogger(GenerateKartaPrzedmiotuPdfService.class);
 
-    private final PrzedmiotRepository przedmiotRepository;
-    private final KartaPrzedmiotuRepository kartaPrzedmiotuRepository;
     private final ZajecieRepository zajecieRepository;
-    private final EfektKsztalceniaRepository efektKsztalceniaRepository;
-    private final ProgramStudiowRepository programStudiowRepository;
-    private final TypStudiowRepository typStudiowRepository;
 
-    public GenerateKartaPrzedmiotuPdfService(PrzedmiotRepository przedmiotRepository,
-                                             KartaPrzedmiotuRepository kartaPrzedmiotuRepository,
-                                             ZajecieRepository zajecieRepository,
-                                             EfektKsztalceniaRepository efektKsztalceniaRepository,
-                                             ProgramStudiowRepository programStudiowRepository,
-                                             TypStudiowRepository typStudiowRepository) {
-        this.przedmiotRepository = przedmiotRepository;
-        this.kartaPrzedmiotuRepository = kartaPrzedmiotuRepository;
+    private Font font12normal = null;
+    private Font font12bold = null;
+
+    public GenerateKartaPrzedmiotuPdfService(ZajecieRepository zajecieRepository) {
         this.zajecieRepository = zajecieRepository;
-        this.efektKsztalceniaRepository = efektKsztalceniaRepository;
-        this.programStudiowRepository = programStudiowRepository;
-        this.typStudiowRepository = typStudiowRepository;
+        createFonts();
     }
 
-    //TODO: rozbic na funkcje ofc
     public File generateKartaPrzedmiotuPdfForPrzedmiot(Optional<Przedmiot> przedmiot) throws NullPointerException {
         if (!przedmiot.isPresent())
             throw new NullPointerException("Przedmiot for PDF creation is not present.");
@@ -58,80 +47,14 @@ public class GenerateKartaPrzedmiotuPdfService {
             PdfWriter.getInstance(document, new FileOutputStream(file));
 
             document.open();
-            BaseFont baseTimesRoman = BaseFont.createFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, false);
-            Font font12normal = new Font(baseTimesRoman, 12, Font.NORMAL, BaseColor.BLACK);
-            Font font12bold = new Font(baseTimesRoman, 12, Font.BOLD, BaseColor.BLACK);
 
-            //1
-            PdfPTable headerTable = new PdfPTable(1);
-            headerTable.setSpacingAfter(20f);
-            addRowTransparentHorizontalBorder(headerTable, font12normal, String.format("WYDZIAŁ %s\n", programStudiow.getWydzial()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("KARTA PRZEDMIOTU %s\n", ""));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Nazwa w jezyku polskim: %s\n", kartaPrzedmiotu.getNazwa()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Nazwa w jezyku angielskim: %s\n", kartaPrzedmiotu.getNazwaAng()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Kierunek studiów (jeśli dotyczy): %s\n", programStudiow.getKierunek()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Specjalność (jeśli dotyczy): %s\n", programStudiow.getSpecjalnosc()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Stopień studiów i forma: %s\n", String.format("%s, %s", typStudiow.getStopienStudiow(), programStudiow.getFormaStudiow())));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Rodzaj przedmiotu: %s\n", kartaPrzedmiotu.getRodzajPrzedmiotu()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Kod przedmiotu: %s\n", kartaPrzedmiotu.getKodPrzedmiotu()));
-            addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Grupa kursów: %s\n", "//TODO"));
-            headerTable.getRow(0).getCells()[0].enableBorderSide(Rectangle.TOP);
-            headerTable.getRow(headerTable.size() - 1).getCells()[0].enableBorderSide(Rectangle.BOTTOM);
+            PdfPTable headerTable = createHeaderTable(programStudiow, kartaPrzedmiotu, typStudiow);
             document.add(headerTable);
 
-            //2
-            PdfPTable formaPrzedmiotuTable = new PdfPTable(6);
-            formaPrzedmiotuTable.setSpacingAfter(20f);
-
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "Wykład");
-            addRow(formaPrzedmiotuTable, font12normal, "Cwiczenia");
-            addRow(formaPrzedmiotuTable, font12normal, "Laboratorium");
-            addRow(formaPrzedmiotuTable, font12normal, "Projekt");
-            addRow(formaPrzedmiotuTable, font12normal, "Seminarium");
-
-            addRow(formaPrzedmiotuTable, font12normal, "Liczba godzin zajęć zorganizowanych w Uczelni (ZZU)");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-
-
-            addRow(formaPrzedmiotuTable, font12normal, "Liczba godzin całkowitego nakładu pracy studenta (CNPS)");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-
-            addRow(formaPrzedmiotuTable, font12normal, "Forma zaliczenia");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-
-            addRow(formaPrzedmiotuTable, font12normal, "Liczba punktów ECTS");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-            addRow(formaPrzedmiotuTable, font12normal, "");
-
+            PdfPTable formaPrzedmiotuTable = createFormaPrzedmiotuTable(zajeciesSet);
             document.add(formaPrzedmiotuTable);
 
-
-            //3
-            PdfPTable pekTable = new PdfPTable(1);
-            addRowTransparentHorizontalBorder(pekTable, font12bold, "PRZEDMIOTOWE EFEKTY KSZTALCENIA");
-
-            for (EfektKsztalcenia ek : efektKsztalceniasSet) {
-                addRowTransparentHorizontalBorder(pekTable, font12normal, String.format("%s %s", ek.getKodEfektu(), ek.getOpis()));
-            }
-
-            pekTable.getRow(0).getCells()[0].enableBorderSide(Rectangle.TOP);
-            pekTable.getRow(pekTable.size() - 1).getCells()[0].enableBorderSide(Rectangle.BOTTOM);
+            PdfPTable pekTable = createPEKTable(efektKsztalceniasSet);
             document.add(pekTable);
 
             document.close();
@@ -140,6 +63,7 @@ public class GenerateKartaPrzedmiotuPdfService {
 
         return file;
     }
+
 
     private static void addRow(PdfPTable table, Font font, String text) {
         table.addCell(new PdfPCell(new Phrase(text, font)));
@@ -150,6 +74,93 @@ public class GenerateKartaPrzedmiotuPdfService {
         pdfPCell.disableBorderSide(Rectangle.TOP);
         pdfPCell.disableBorderSide(Rectangle.BOTTOM);
         table.addCell(pdfPCell);
+    }
+
+    private PdfPTable createHeaderTable(ProgramStudiow programStudiow, KartaPrzedmiotu kartaPrzedmiotu, TypStudiow typStudiow) {
+        PdfPTable headerTable = new PdfPTable(1);
+        headerTable.setSpacingAfter(20f);
+
+        addRowTransparentHorizontalBorder(headerTable, font12normal, String.format("WYDZIAŁ %s\n", programStudiow.getWydzial()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("KARTA PRZEDMIOTU %s\n", ""));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Nazwa w jezyku polskim: %s\n", kartaPrzedmiotu.getNazwa()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Nazwa w jezyku angielskim: %s\n", kartaPrzedmiotu.getNazwaAng()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Kierunek studiów (jeśli dotyczy): %s\n", programStudiow.getKierunek()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Specjalność (jeśli dotyczy): %s\n", programStudiow.getSpecjalnosc()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Stopień studiów i forma: %s\n", String.format("%s, %s", typStudiow.getStopienStudiow(), programStudiow.getFormaStudiow())));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Rodzaj przedmiotu: %s\n", kartaPrzedmiotu.getRodzajPrzedmiotu()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Kod przedmiotu: %s\n", kartaPrzedmiotu.getKodPrzedmiotu()));
+        addRowTransparentHorizontalBorder(headerTable, font12bold, String.format("Grupa kursów: %s\n", "//TODO"));
+
+        headerTable.getRow(0).getCells()[0].enableBorderSide(Rectangle.TOP);
+        headerTable.getRow(headerTable.size() - 1).getCells()[0].enableBorderSide(Rectangle.BOTTOM);
+        return headerTable;
+    }
+
+    //TODO: wypelnic te tabele
+    private PdfPTable createFormaPrzedmiotuTable(Set<Zajecie> zajecieSet) {
+        PdfPTable formaPrzedmiotuTable = new PdfPTable(6);
+        formaPrzedmiotuTable.setSpacingAfter(20f);
+
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "Wykład");
+        addRow(formaPrzedmiotuTable, font12normal, "Cwiczenia");
+        addRow(formaPrzedmiotuTable, font12normal, "Laboratorium");
+        addRow(formaPrzedmiotuTable, font12normal, "Projekt");
+        addRow(formaPrzedmiotuTable, font12normal, "Seminarium");
+
+        addRow(formaPrzedmiotuTable, font12normal, "Liczba godzin zajęć zorganizowanych w Uczelni (ZZU)");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+
+
+        addRow(formaPrzedmiotuTable, font12normal, "Liczba godzin całkowitego nakładu pracy studenta (CNPS)");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+
+        addRow(formaPrzedmiotuTable, font12normal, "Forma zaliczenia");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+
+        addRow(formaPrzedmiotuTable, font12normal, "Liczba punktów ECTS");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+        addRow(formaPrzedmiotuTable, font12normal, "");
+
+        return formaPrzedmiotuTable;
+    }
+
+    private PdfPTable createPEKTable(Set<EfektKsztalcenia> efektKsztalceniasSet) {
+        PdfPTable pekTable = new PdfPTable(1);
+        addRowTransparentHorizontalBorder(pekTable, font12bold, "PRZEDMIOTOWE EFEKTY KSZTAŁCENIA");
+
+        for (EfektKsztalcenia ek : efektKsztalceniasSet) {
+            addRowTransparentHorizontalBorder(pekTable, font12normal, String.format("%s %s", ek.getKodEfektu(), ek.getOpis()));
+        }
+        pekTable.getRow(0).getCells()[0].enableBorderSide(Rectangle.TOP);
+        pekTable.getRow(pekTable.size() - 1).getCells()[0].enableBorderSide(Rectangle.BOTTOM);
+
+        return pekTable;
+    }
+
+    private void createFonts() {
+        try {
+            BaseFont baseTimesRoman = BaseFont.createFont(BaseFont.TIMES_ROMAN, BaseFont.CP1250, false);
+            font12normal = new Font(baseTimesRoman, 12, Font.NORMAL, BaseColor.BLACK);
+            font12bold = new Font(baseTimesRoman, 12, Font.BOLD, BaseColor.BLACK);
+        } catch (Exception e) {
+            log.error("Failed creating fonts {}", e.toString());
+        }
     }
 
 }
